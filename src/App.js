0d6042f5f224bcf4fa3e3a75d0b3fd19fa97b03d
Arjun Sharma <144344598+Arjunsharmapy@users.noreply.github.com>
2024-09-21 23:53:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Peer from "simple-peer";
 import io from "socket.io-client";
 import "./App.css";
 
-const socket = io.connect("https://0463-103-80-23-90.ngrok-free.app");
+const socket = io.connect("https://signaling-server-kwrs.onrender.com");
 function App() {
   const [me, setMe] = useState("");
   const [stream, setStream] = useState();
